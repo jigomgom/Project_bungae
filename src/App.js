5629ce4fa7e60f83d49445610612ac7ace3ae4bc
jigomgom <dydrkfl5519@gmail.com>
2022-07-29 19:54:52 +0900

    Notfound 페이지 미적용 수정

--- src/App.js
+++ src/App.js
@@ -67,7 +67,7 @@ function App() {
         <Route path="/oauth" element={<LoadingLogin />} />
         <Route path="/signup" element={<SignUp />} />
         {/* Not Found */}
-        {/* <Route path="*" element={<NotFound />} /> */}
+        <Route path="*" element={<NotFound />} />
         <Route path="/on" element={<OnBoarding />} />
       </Routes>
       {/* <Footer /> */}
